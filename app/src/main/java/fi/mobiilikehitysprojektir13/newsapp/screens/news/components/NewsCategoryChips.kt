fc381307c4d842fadd73c9c9ef8cf164788ae440
syorito-hatsuki <enderdron3@gmail.com>
2024-03-20 05:44:09 +0200

    Refactored CategoryChips

--- app/src/main/java/fi/mobiilikehitysprojektir13/newsapp/screens/news/components/NewsCategoryChips.kt
+++ app/src/main/java/fi/mobiilikehitysprojektir13/newsapp/screens/news/components/NewsCategoryChips.kt
@@ -1,63 +1,55 @@
 package fi.mobiilikehitysprojektir13.newsapp.screens.news.components
 
-import android.util.Log
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Done
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun CategoryChips(categories: List<String>) {
-    var selectedCategory by remember { mutableStateOf<String?>(null) }
+fun CategoryChips(onChange: (categories: Set<String>) -> Unit) {
+
+    val selectedCategories = remember { mutableStateListOf<String>() }
 
     Row(
         modifier = Modifier
-            .padding(horizontal = 8.dp)
             .horizontalScroll(rememberScrollState())
+            .padding(horizontal = 8.dp)
     ) {
-        categories.forEach { category ->
-            val isSelected = category == selectedCategory
-            FilterChip(
-                onClick = {
-                    selectedCategory = if (isSelected) null else category
-                    Log.d("CategoryChips", "Selected category: $selectedCategory")
-                },
-                label = {
-                    Text(
-                        text = category,
-                        color = if (isSelected) Color.Black else Color.White
-                    )
-                },
-                selected = isSelected,
-                leadingIcon = if (isSelected) {
-                    {
-                        Icon(
-                            imageVector = Icons.Filled.Done,
-                            contentDescription = "Done icon",
-                            tint = if (isSelected) Color.Black else Color.White,
-                            modifier = Modifier.size(FilterChipDefaults.IconSize)
+        setOf(
+            "Politics", "Business", "Technology", "Science", "Health", "Entertainment", "Sports"
+        ).forEach { category ->
+            val isSelected = selectedCategories.contains(category)
+            FilterChip(onClick = {
+                when {
+                    isSelected -> selectedCategories.remove(category)
+                    else -> selectedCategories.add(category)
+                }
+                onChange.invoke(selectedCategories.toSet())
+            }, label = {
+                Text(text = category, color = if (isSelected) Color.Black else Color.White)
+            }, selected = isSelected, leadingIcon = {
+                if (isSelected) Icon(
+                    imageVector = Icons.Filled.Done,
+                    contentDescription = "Done icon",
+                    tint = Color.Black,
+                    modifier = Modifier.size(FilterChipDefaults.IconSize)
 
-                        )
-                    }
-                } else {
-                    null
-                },
-                modifier = Modifier.padding(horizontal = 2.dp)
-            )
+                )
+            }, modifier = Modifier.padding(horizontal = 2.dp))
         }
     }
 }
@@ -65,8 +57,8 @@ fun CategoryChips(categories: List<String>) {
 @Preview
 @Composable
 fun PreviewCategoryChips() {
-    val categories =
-        listOf("Politics", "Business", "Technology", "Science", "Health", "Entertainment", "Sports")
-    CategoryChips(categories)
+    CategoryChips {
+
+    }
 }
 
