7b2f57405350e30862877edcbdb1efaf9b30ae2c
syorito-hatsuki <enderdron3@gmail.com>
2024-03-20 05:42:06 +0200

    Updated API implementation

--- app/src/main/java/fi/mobiilikehitysprojektir13/newsapp/data/api/NewsDataApi.kt
+++ app/src/main/java/fi/mobiilikehitysprojektir13/newsapp/data/api/NewsDataApi.kt
@@ -3,5 +3,10 @@ package fi.mobiilikehitysprojektir13.newsapp.data.api
 import fi.mobiilikehitysprojektir13.newsapp.data.dto.News
 
 interface NewsDataApi {
-    suspend fun getLatestNews(): News
+    suspend fun getLatestNews(
+        query: String = "",
+        categories: Set<String> = emptySet(),
+        countries: Set<String> = emptySet(),
+        languages: Set<String> = emptySet()
+    ): News
 }
