ac8faf53c2e8fe8499df66400e5908f4cc56e423
syorito-hatsuki <enderdron3@gmail.com>
2024-03-28 22:21:01 +0200

    Replaced Row + Button with NavigationBar + NavigationBarItem

---
+++ app/src/main/java/fi/mobiilikehitysprojektir13/newsapp/BottomNavigationItem.kt
@@ -0,0 +1,31 @@
+package fi.mobiilikehitysprojektir13.newsapp
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class BottomNavigationItem(
+    val label: String = "",
+    val icon: ImageVector = Icons.Filled.Home,
+    val route: String = ""
+) {
+    fun bottomNavigationItems(): List<BottomNavigationItem> = listOf(
+        BottomNavigationItem(
+            label = "News",
+            icon = Icons.Filled.Home,
+            route = Screens.News.route
+        ),
+        BottomNavigationItem(
+            label = "Favorite",
+            icon = Icons.Filled.Star,
+            route = Screens.Favorite.route
+        ),
+        BottomNavigationItem(
+            label = "Settings",
+            icon = Icons.Filled.Settings,
+            route = Screens.Settings.route
+        ),
+    )
+}
--- app/src/main/java/fi/mobiilikehitysprojektir13/newsapp/components/BottomBar.kt
+++ app/src/main/java/fi/mobiilikehitysprojektir13/newsapp/components/BottomBar.kt
@@ -1,61 +1,57 @@
 package fi.mobiilikehitysprojektir13.newsapp.components
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.currentBackStackEntryAsState
+import fi.mobiilikehitysprojektir13.newsapp.BottomNavigationItem
+import fi.mobiilikehitysprojektir13.newsapp.ui.theme.Blue
+import fi.mobiilikehitysprojektir13.newsapp.ui.theme.Dark
 
 @Composable
-fun BottomBar (navController: NavController) {
-    Row {
-        NavigationButton(
-            icon = Icons.Default.Home,
-            contentDescription = "Home icon",
-            onClick = {
-            /* navigate */ }
-        )
-        Spacer(modifier = Modifier.width(16.dp))
-        NavigationButton(
-            icon = Icons.Default.Star,
-            contentDescription = "Starred icon",
-            onClick = { /* navigate */ }
-        )
-        Spacer(modifier = Modifier.width(16.dp))
-        NavigationButton(
-            icon = Icons.Default.Settings,
-            contentDescription = "Settings icon",
-            onClick = {  }
-        )
+fun BottomBar(navController: NavController) {
+
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+
+    if (!BottomNavigationItem().bottomNavigationItems().any {
+            it.route == currentDestination?.route
+        }) return
+
+    NavigationBar {
+        BottomNavigationItem().bottomNavigationItems().forEach { navigationItem ->
+            NavigationBarItem(selected = navigationItem.route == currentDestination?.route,
+                label = {
+                    Text(navigationItem.label)
+                },
+                icon = {
+                    Icon(navigationItem.icon, navigationItem.label)
+                },
+                onClick = {
+                    navController.popBackStack()
+                    navController.navigate(navigationItem.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                },
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = Blue,
+                    selectedTextColor = Blue,
+                    indicatorColor = Dark,
+                    unselectedIconColor = Color.LightGray,
+                    unselectedTextColor = Color.LightGray,
+                )
+            )
+        }
     }
 }
-
-@Composable
-fun NavigationButton(
-    icon: ImageVector,
-    contentDescription: String,
-    onClick: () -> Unit
-) {
-    Button(
-        onClick = onClick,
-        shape = LessRoundedShape
-    ) {
-        Icon(
-            imageVector = icon,
-            contentDescription = contentDescription,
-            modifier = Modifier.size(48.dp)
-        )
-    }
-}
-
-private val LessRoundedShape = androidx.compose.foundation.shape.RoundedCornerShape(4.dp)
