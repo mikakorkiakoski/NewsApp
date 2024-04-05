698898f2a1efaebc707cc1fa0db1a9ab8fdb9b64
syorito-hatsuki <enderdron3@gmail.com>
2024-04-05 23:03:23 +0300

    Added map screen to NavigationHost

--- app/src/main/java/fi/mobiilikehitysprojektir13/newsapp/Screens.kt
+++ app/src/main/java/fi/mobiilikehitysprojektir13/newsapp/Screens.kt
@@ -1,6 +1,7 @@
 package fi.mobiilikehitysprojektir13.newsapp
 
 sealed class  Screens(val route: String, val argument: String = "") {
+    data object Map : Screens("map")
     data object News : Screens("news")
     data object Article : Screens("article/{articleId}", "articleId")
     data object Favorite : Screens("favorite")
--- app/src/main/java/fi/mobiilikehitysprojektir13/newsapp/ui/theme/components/NavigationHost.kt
+++ app/src/main/java/fi/mobiilikehitysprojektir13/newsapp/ui/theme/components/NavigationHost.kt
@@ -4,27 +4,39 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import fi.mobiilikehitysprojektir13.newsapp.Screens
+import fi.mobiilikehitysprojektir13.newsapp.data.store.UserSettingsStore
 import fi.mobiilikehitysprojektir13.newsapp.screens.article.ArticleScreen
+import fi.mobiilikehitysprojektir13.newsapp.screens.country.CountrySelectionScreen
 import fi.mobiilikehitysprojektir13.newsapp.screens.news.NewsScreen
 import fi.mobiilikehitysprojektir13.newsapp.screens.settings.SettingsScreen
 
 @Composable
 fun NavigationHost(
-    navController: NavHostController,
-    paddingValues: PaddingValues
+    navController: NavHostController, paddingValues: PaddingValues
 ) {
+
+    val context = LocalContext.current
+    val userSettingsStore = UserSettingsStore(context)
+    val country = userSettingsStore.getCountry.collectAsState(initial = "")
+
     NavHost(
-        navController = navController,
-        startDestination = Screens.News.route,
-        modifier = Modifier.padding(paddingValues = paddingValues)
+        navController = navController, startDestination = when {
+            country.value.isNotBlank() -> Screens.News.route
+            else -> Screens.Map.route
+        }, modifier = Modifier.padding(paddingValues = paddingValues)
     ) {
+        composable(Screens.Map.route) {
+            CountrySelectionScreen(navController)
+        }
         composable(Screens.News.route) {
             NewsScreen(navController)
         }
@@ -35,7 +47,7 @@ fun NavigationHost(
             ArticleScreen(navController, it)
         }
         composable(Screens.Settings.route) {
-            SettingsScreen()
+            SettingsScreen(navController)
         }
         composable(Screens.Favorite.route) {
             Text(text = "Favorites? :)")
