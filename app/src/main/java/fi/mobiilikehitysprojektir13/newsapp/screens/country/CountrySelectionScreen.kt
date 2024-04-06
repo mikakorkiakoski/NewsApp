32c1ce23b5952e1b29cb923497de5864b389e63a
syorito-hatsuki <enderdron3@gmail.com>
2024-04-06 20:11:40 +0300

    Implemented RestCountries and fu*ked up with multi-typing

--- app/src/main/java/fi/mobiilikehitysprojektir13/newsapp/screens/country/CountrySelectionScreen.kt
+++ app/src/main/java/fi/mobiilikehitysprojektir13/newsapp/screens/country/CountrySelectionScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -24,39 +25,30 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import fi.mobiilikehitysprojektir13.newsapp.Screens
+import fi.mobiilikehitysprojektir13.newsapp.data.countries
+import fi.mobiilikehitysprojektir13.newsapp.data.dto.Country
 import fi.mobiilikehitysprojektir13.newsapp.data.languages
 import fi.mobiilikehitysprojektir13.newsapp.data.store.UserSettingsStore
+import fi.mobiilikehitysprojektir13.newsapp.screens.country.components.CountryList
 import fi.mobiilikehitysprojektir13.newsapp.screens.country.components.Location
-import fi.mobiilikehitysprojektir13.newsapp.screens.country.components.LocationList
 import fi.mobiilikehitysprojektir13.newsapp.screens.country.components.LocationStatus
 import fi.mobiilikehitysprojektir13.newsapp.screens.country.components.Map
 import fi.mobiilikehitysprojektir13.newsapp.screens.country.viewmodel.CountrySelectionViewModel
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.json.Json
 import org.osmdroid.util.GeoPoint
 
-@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("CoroutineCreationDuringComposition")
 @Composable
 fun CountrySelectionScreen(navController: NavHostController) {
     val context = LocalContext.current
     val userSettingsStore = UserSettingsStore(context)
     val country = userSettingsStore.getCountry.collectAsState(initial = "")
-
-    val scope = rememberCoroutineScope()
-
-    val countrySelectionViewModel: CountrySelectionViewModel = viewModel()
-
-    val (selectedOption, onOptionSelected) = remember { mutableStateOf("") }
-    val modeMap = remember { mutableStateOf(false) }
-    val geoPoint = remember { mutableStateOf<GeoPoint>(GeoPoint(0.0, 0.0)) }
-
-    val buttonText = when {
-        modeMap.value -> "Switch to List"
-        else -> "Switch to Map"
-    }
 
     Location {
         when (it.status) {
@@ -68,98 +60,168 @@ fun CountrySelectionScreen(navController: NavHostController) {
                 CircularProgressIndicator()
             }
 
-            LocationStatus.DENIED -> Scaffold(topBar = {
-                CenterAlignedTopAppBar(title = { Text(text = "Select Country") })
-            }, bottomBar = {
-                BottomAppBar {
-                    Button(modifier = Modifier
-                        .fillMaxWidth(0.5f)
-                        .padding(start = 8.dp, end = 4.dp),
-                        onClick = {
-                            modeMap.value = !modeMap.value
-                        }) {
-                        Text(text = buttonText)
-                    }
-                    Button(modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(start = 4.dp, end = 8.dp),
-                        onClick = {
-                            scope.launch {
-                                when {
-                                    modeMap.value -> countrySelectionViewModel.getCountry(
-                                        latitude = geoPoint.value.latitude,
-                                        longitude = geoPoint.value.longitude
-                                    ).catch {
-                                        Toast.makeText(
-                                            context,
-                                            "Can't get access to Reverse Geo API",
-                                            Toast.LENGTH_SHORT
-                                        ).show()
-                                    }.collect { locationCode: String? ->
-                                        userSettingsStore.saveCountry(
-                                            locationCode?.lowercase() ?: "en"
-                                        )
-                                    }
+            LocationStatus.DENIED -> Selector(navController, GeoPoint(0.0, 0.0))
 
-                                    else -> {
-                                        if (selectedOption.isBlank()) {
-                                            Toast.makeText(
-                                                context,
-                                                "Please select any country",
-                                                Toast.LENGTH_SHORT
-                                            ).show()
-                                            return@launch
-                                        }
-                                        userSettingsStore.saveCountry(
-                                            languages[selectedOption] ?: "en"
-                                        )
-                                    }
-                                }
-                            }
-                            if (country.value.isNotBlank()) {
-                                navController.navigate(Screens.News.route) {
-                                    popUpTo(navController.graph.id) {
-                                        inclusive = true
-                                    }
-                                }
-                            }
-                        }) {
-                        Text(text = "Confirm")
-                    }
-                }
-            }) { paddingValues ->
-                Column(modifier = Modifier.padding(paddingValues)) {
-                    when {
-                        modeMap.value -> Map { point ->
-                            geoPoint.value = point
-                        }
-
-                        else -> LocationList(selectedOption, onOptionSelected)
-                    }
-                }
-            }
-
-            LocationStatus.SUCCESS -> {
-                scope.launch {
-                    countrySelectionViewModel.getCountry(
-                        latitude = it.location?.latitude ?: 0.0,
-                        longitude = it.location?.longitude ?: 0.0
-                    ).catch {
-                        Toast.makeText(
-                            context, "Can't get access to Reverse Geo API", Toast.LENGTH_SHORT
-                        ).show()
-                    }.collect { locationCode ->
-                        userSettingsStore.saveCountry(locationCode.lowercase())
-                    }
-                }
+            LocationStatus.SUCCESS -> Selector(
+                navController, GeoPoint(it.location?.latitude ?: 0.0, it.location?.longitude ?: 0.0)
+            )
+        }
+        if (country.value.isNotBlank()) navController.navigate(Screens.News.route) {
+            popUpTo(navController.graph.id) {
+                inclusive = true
             }
         }
-        if (country.value.isNotBlank()) {
-            navController.navigate(Screens.News.route) {
-                popUpTo(navController.graph.id) {
-                    inclusive = true
-                }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Selector(navController: NavController, initGeoPoint: GeoPoint) {
+    val context = LocalContext.current
+    val userSettingsStore = UserSettingsStore(context)
+    val country = userSettingsStore.getCountry.collectAsState(initial = "")
+
+    val scope = rememberCoroutineScope()
+
+    val countrySelectionViewModel: CountrySelectionViewModel = viewModel()
+
+    val (selectedOption, onOptionSelected) = remember { mutableStateOf("") }
+    val modeMap = remember { mutableStateOf(false) }
+    val geoPoint = remember { mutableStateOf(initGeoPoint) }
+
+    LaunchedEffect("get_country") {
+        if (geoPoint.value.latitude != 0.0 && geoPoint.value.longitude != 0.0) countrySelectionViewModel.getCountry(
+            geoPoint.value.latitude, geoPoint.value.longitude
+        ).catch {
+            Toast.makeText(
+                context,
+                "Can't get access to Reverse Geo API or Location is incorrect",
+                Toast.LENGTH_SHORT
+            ).show()
+        }.collect { country ->
+            countries.entries.find { it.value == country.lowercase() }?.let {
+                onOptionSelected(it.key)
             }
+        }
+    }
+
+    Scaffold(topBar = {
+        CenterAlignedTopAppBar(title = { Text(text = "Select Country") })
+    }, bottomBar = {
+        BottomAppBar {
+            Button(modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .padding(start = 8.dp, end = 4.dp),
+                onClick = {
+                    modeMap.value = !modeMap.value
+                }) {
+                Text(
+                    text = when {
+                        modeMap.value -> "Switch to List"
+                        else -> "Switch to Map"
+                    }
+                )
+            }
+            Button(modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 4.dp, end = 8.dp), onClick = {
+                scope.launch {
+                    when {
+                        modeMap.value -> countrySelectionViewModel.getCountry(
+                            latitude = geoPoint.value.latitude,
+                            longitude = geoPoint.value.longitude
+                        ).catch {
+                            Toast.makeText(
+                                context,
+                                "Can't get access to Reverse Geo API or Location is incorrect",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }.collect { locationCode: String ->
+                            countrySelectionViewModel.getLanguageByCountryCode(
+                                locationCode
+                            ).catch {
+                                Toast.makeText(
+                                    context,
+                                    "Can't get access to RestCountries API or Location is incorrect",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }.collect { country ->
+                                parseCountry(country, userSettingsStore)
+                            }
+                            userSettingsStore.saveCountry(
+                                countries.entries.firstOrNull { entry ->
+                                    entry.value == locationCode.lowercase()
+                                }?.value ?: "us"
+                            )
+                        }
+
+                        else -> {
+                            if (selectedOption.isBlank()) {
+                                Toast.makeText(
+                                    context, "Please select any country", Toast.LENGTH_SHORT
+                                ).show()
+                                return@launch
+                            }
+
+                            countrySelectionViewModel.getLanguageByName(
+                                selectedOption
+                            ).catch {
+                                Toast.makeText(
+                                    context,
+                                    "Can't get access to RestCountries API or Location is incorrect",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }.collect { country ->
+                                parseCountry(country, userSettingsStore)
+                            }
+                            userSettingsStore.saveCountry(
+                                countries[selectedOption] ?: "us"
+                            )
+                        }
+                    }
+                }
+                if (country.value.isNotBlank()) {
+                    navController.navigate(Screens.News.route) {
+                        popUpTo(navController.graph.id) {
+                            inclusive = true
+                        }
+                    }
+                }
+            }) {
+                Text(text = "Confirm")
+            }
+        }
+    }) { paddingValues ->
+        Column(modifier = Modifier.padding(paddingValues)) {
+            when {
+                modeMap.value -> Map { point ->
+                    geoPoint.value = point
+                }
+
+                else -> CountryList(selectedOption, onOptionSelected)
+            }
+        }
+    }
+}
+
+suspend fun parseCountry(country: String, userSettingsStore: UserSettingsStore) {
+    try {
+        val list = Json.decodeFromString<List<Country>>(country)
+        userSettingsStore.saveLanguage(
+            languages[list.firstOrNull()?.languages?.entries?.find { entry ->
+                languages.containsKey(entry.value)
+            }?.value] ?: "en"
+        )
+    } catch (e: SerializationException) {
+        try {
+            val single = Json.decodeFromString<Country>(country)
+            userSettingsStore.saveLanguage(
+                languages[single.languages.entries.find { entry ->
+                    languages.containsKey(entry.value)
+                }?.value] ?: "en"
+            )
+        } catch (e: SerializationException) {
+            throw IllegalStateException("Unknown response structure")
         }
     }
 }
--- app/src/main/java/fi/mobiilikehitysprojektir13/newsapp/screens/country/viewmodel/CountrySelectionViewModel.kt
+++ app/src/main/java/fi/mobiilikehitysprojektir13/newsapp/screens/country/viewmodel/CountrySelectionViewModel.kt
@@ -1,15 +1,25 @@
 package fi.mobiilikehitysprojektir13.newsapp.screens.country.viewmodel
 
 import androidx.lifecycle.ViewModel
+import fi.mobiilikehitysprojektir13.newsapp.data.api.countries.RestCountriesApi
 import fi.mobiilikehitysprojektir13.newsapp.data.api.weather.OpenWeatherMapApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import org.koin.java.KoinJavaComponent.inject
 
 class CountrySelectionViewModel : ViewModel() {
-    private val api: OpenWeatherMapApi by inject(OpenWeatherMapApi::class.java)
+    private val weatherMapApi: OpenWeatherMapApi by inject(OpenWeatherMapApi::class.java)
+    private val restCountriesApi: RestCountriesApi by inject(RestCountriesApi::class.java)
 
     fun getCountry(latitude: Double, longitude: Double): Flow<String> = flow {
-        emit(api.getCountry(latitude, longitude).first().country)
+        emit(weatherMapApi.getCountry(latitude, longitude).first().country)
+    }
+
+    fun getLanguageByCountryCode(code: String): Flow<String> = flow {
+        emit(restCountriesApi.getByCode(code))
+    }
+
+    fun getLanguageByName(country: String): Flow<String> = flow {
+        emit(restCountriesApi.getByCountryName(country))
     }
 }
