e13d1b0327aa86d64c01d8a8a7dc77534e8d5089
asrii00 <t2rias00@students.oamk.fi>
2024-04-09 10:41:43 +0300

    added save article button (no implementation)

--- app/src/main/java/fi/mobiilikehitysprojektir13/newsapp/screens/article/ArticleScreen.kt
+++ app/src/main/java/fi/mobiilikehitysprojektir13/newsapp/screens/article/ArticleScreen.kt
@@ -1,18 +1,29 @@
 package fi.mobiilikehitysprojektir13.newsapp.screens.article
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material3.AssistChip
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -68,8 +79,45 @@ fun ArticleScreen(navController: NavController, navBackStackEntry: NavBackStackE
                     article?.description?.let {
                         Text(text = it)
                     }
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .border(
+                                    border = BorderStroke(1.dp, Color.Gray),
+                                    shape = RoundedCornerShape(4.dp)
+                                )
+                                .padding(4.dp)
+                        ) {
+                            Row(verticalAlignment = Alignment.CenterVertically) {
+                                IconButton(
+                                    onClick = {saveArticle(articleId)}
+                                ) {
+                                    Icon(
+                                        Icons.Outlined.FavoriteBorder, //there's no outlined star icon?
+                                        contentDescription = null,
+                                        Modifier.size(32.dp))
+                                }
+                                Text(
+                                    text = "Save article",
+                                    color = Color.Gray,
+                                    modifier = Modifier
+                                        .padding(16.dp)
+                                )
+                            }
+                        }
+                    }
+                    
                 }
             }
         }
     }
+}
+
+fun saveArticle(id: String){
+
 }
