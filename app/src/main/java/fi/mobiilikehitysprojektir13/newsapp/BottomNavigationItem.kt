4a06cda68402268b3b324084b81cd2694da316b0
mikakorkiakoski <mika.korkiakoski@icloud.com>
2024-04-22 18:39:25 +0300

    Replaced Home icon with Newspaper icon

--- app/src/main/java/fi/mobiilikehitysprojektir13/newsapp/BottomNavigationItem.kt
+++ app/src/main/java/fi/mobiilikehitysprojektir13/newsapp/BottomNavigationItem.kt
@@ -4,17 +4,20 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.Star
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 
 data class BottomNavigationItem(
     val label: String = "",
     val icon: ImageVector = Icons.Filled.Home,
     val route: String = ""
 ) {
+    @Composable
     fun bottomNavigationItems(): List<BottomNavigationItem> = listOf(
         BottomNavigationItem(
             label = "News",
-            icon = Icons.Filled.Home,
+            icon = ImageVector.vectorResource(R.drawable.baseline_newspaper_24),
             route = Screens.News.route
         ),
         BottomNavigationItem(
